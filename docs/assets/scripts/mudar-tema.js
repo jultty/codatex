101e53de2cf36b910ff358e020d80b0c11b604ef
jultty <juffff@tutanota.de>
2022-05-18 17:00:58 -0300

    Persiste preferência de tema
    Este commit faz com que a preferência do tema
    (escuro ou claro) seja gravada no navegador
    usando o recurso localStorage.
    
    Define uma nova lógica para o arquivo mudar-tema.js
    verificando se há ou não uma preferência do navegador
    por temas escuros.
    
    Outras alterações:
    
        - Altera cor do botão na página principal de roxo para verde
    	- Comenta dois links svg que faziam a tela piscar nas trocas de
    páginas
    	- Retira código legado de localização do texto placeholder na barra
    de pesquisa
    	- Adiciona webrick 1.7 à Gemfile

--- docs/assets/scripts/mudar-tema.js
+++ docs/assets/scripts/mudar-tema.js
@@ -1,16 +1,32 @@
 const toggleDarkMode = document.querySelector('.js-mudar-tema');
+var storedTheme = localStorage.getItem('storedTheme');
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    jtd.setTheme('dark');
+// sem configurações de tema pré-configuradas ou fornecidas
+if (storedTheme == undefined) {
+    if (!(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        jtd.setTheme('light');
+    }
 }
 
-jtd.addEvent(toggleDarkMode, 'click', function(){
-  if (jtd.getTheme() === 'dark') {
+// configurações de tema fornecidas anteriormente
+if (storedTheme == 'light') {
     jtd.setTheme('light');
-    //toggleDarkMode.textContent = 'Tema escuro';
+}
+
+// interação com botão de troca do tema
+jtd.addEvent(toggleDarkMode, 'click', function(){
+
+    // tema atual é o padrão (escuro)
+    if (jtd.getTheme() === 'dark' || jtd.getTheme() === 'default') {
+    jtd.setTheme('light');
+    localStorage.setItem('storedTheme', 'light');
+    let storedTheme = localStorage.getItem('storedTheme');
+
+  // tema atual é claro
   } else {
     jtd.setTheme('dark');
-    //toggleDarkMode.textContent = 'Tema claro';
+    localStorage.setItem('storedTheme', 'dark');
+    let storedTheme = localStorage.getItem('storedTheme');
   }
 });
 
